3bdd998739d5c39616df124619de581d238cd072
ravi-sidharth <you@example.com>
2025-08-30 00:43:41 +0530

    update

--- server/src/helpers/razorpay.js
+++ server/src/helpers/razorpay.js
@@ -1,8 +1,7 @@
-
 const Razorpay = require('razorpay')
 const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
-module.exports = razorpay
+module.exports = razorpay;
